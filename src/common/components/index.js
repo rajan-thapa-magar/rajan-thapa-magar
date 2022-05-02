e60bbdf847ee8dbe097309641ffbb9cecb9100cb
Rajan Magar <rajan.magar@logicabeans.com>
2022-05-02 11:30:09 +0545

    Updates

--- src/common/components/index.js
+++
@@ -1,3 +0,0 @@
-export * from './button/Button';
-export * from './theme/context';
-export * from './theme-toogle/ThemeToggle';
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export * from './about/About';
-export * from './intro/Intro';
-export * from './skills/Skills';
-export * from './contact/Contact';
-export * from './projects/Projects';
-
